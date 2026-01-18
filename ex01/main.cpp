5e3ed7b8d6bf3f1c8982ea8e65f571b356a7057c
ImSqueezy <ouaallaabdelali1@gmail.com>
2026-01-18 09:52:58 +0100

    Reviewed and Added ex01/

---
+++ ex01/main.cpp
@@ -0,0 +1,53 @@
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+#define NUM_ANIMALS 10
+
+// void	print_ideas(Animal **animals)
+// {
+// 	for (int i = 0; i < NUM_ANIMALS; i++)
+// 	{
+// 		if (dynamic_cast<Dog*>(animals[i]))
+// 			std::cout << dynamic_cast<Dog*>(animals[i])->getBrain()->ideas[i] << std::endl;
+// 		else if (dynamic_cast<Cat*>(animals[i]))
+// 			std::cout << dynamic_cast<Cat*>(animals[i])->getBrain()->ideas[i] << std::endl;
+// 	}
+// }
+
+int main()
+{
+	std::cout << "--- Array ---" << std::endl;
+	Animal *animals[NUM_ANIMALS];
+
+	for (int i = 0; i < NUM_ANIMALS; i++)
+	{
+		if (i < NUM_ANIMALS / 2)
+		{
+			animals[i] = new Dog();
+			dynamic_cast<Dog*>(animals[i])->getBrain()->ideas[i] = "Dog Idea ";
+		}
+		else
+		{
+			animals[i] = new Cat();
+			dynamic_cast<Cat*>(animals[i])->getBrain()->ideas[i] = "Cat Idea ";
+		}
+	}
+
+	// std::cout << "--- Ideas ---" << std::endl;
+	// print_ideas(animals);
+
+	std::cout << "--- Deep Copy Test ---" << std::endl;
+	dynamic_cast<Dog*>(animals[0])->getBrain()->ideas[0] = "Original Idea";
+	{
+		Dog tmp = *dynamic_cast<Dog*>(animals[0]);
+		tmp.getBrain()->ideas[0] = "New Idea";
+		std::cout << "Tmp Idea: " << tmp.getBrain()->ideas[0] << std::endl;
+	}
+
+	std::cout << "Dog Idea: " << dynamic_cast<Dog*>(animals[0])->getBrain()->ideas[0] << std::endl;
+
+	std::cout << "--- Deleting Animals ---" << std::endl;
+	for (int i = 0; i < NUM_ANIMALS; i++)
+		delete animals[i];
+	return 0;
+}
